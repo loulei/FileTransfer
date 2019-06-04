4dd46eb04d89b1191514d30f54e9dea55c330b74
loulei <constantlike@gmail.com>
2019-06-04 15:33:36 +0800

    add progressbar

--- app/src/main/java/com/example/filetransfer/FileTransferService.java
+++ app/src/main/java/com/example/filetransfer/FileTransferService.java
@@ -11,11 +11,17 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import android.annotation.SuppressLint;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class FileTransferService extends IntentService {
@@ -31,39 +37,46 @@ public class FileTransferService extends IntentService {
 	private static final int FLAG_SEND_SUCCESS = 1;
 	private static final int FLAG_RECV_SUCCESS = 2;
 	private static final int FLAG_READY_TO_SERVER = 3;
+	private static final int FLAG_TRANSFER_PROGRESS = 4;
+	private static final int FLAG_SHOW_NOTIFICATION = 5;
+	private static final int FLAG_CANCEL_NOTIFICATION = 6;
 
-    private MyHandler handler = new MyHandler(this);
 
-	private static class MyHandler extends  Handler{
-	    private WeakReference<FileTransferService> fileTransferService;
-
-        public MyHandler(FileTransferService service) {
-            fileTransferService = new WeakReference<>(service);
-        }
-
+    private Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-            FileTransferService service = fileTransferService.get();
-            if (service != null) {
-                switch (msg.what) {
-                    case FLAG_ERROR:
-                        Toast.makeText(service.getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
-                        break;
-                    case FLAG_SEND_SUCCESS:
-                        Toast.makeText(service.getApplicationContext(), "send finish", Toast.LENGTH_SHORT).show();
-                        break;
-                    case FLAG_RECV_SUCCESS:
-                        Toast.makeText(service.getApplicationContext(), "recv finish", Toast.LENGTH_SHORT).show();
-                        break;
-                    case FLAG_READY_TO_SERVER:
-                        Toast.makeText(service.getApplicationContext(), "server ready", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
+            switch (msg.what) {
+                case FLAG_ERROR:
+                    Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                    break;
+                case FLAG_SEND_SUCCESS:
+                	complete();
+                    Toast.makeText(getApplicationContext(), "send finish", Toast.LENGTH_SHORT).show();
+                    break;
+                case FLAG_RECV_SUCCESS:
+                	complete();
+                    Toast.makeText(getApplicationContext(), "recv finish", Toast.LENGTH_SHORT).show();
+                    break;
+                case FLAG_READY_TO_SERVER:
+                    Toast.makeText(getApplicationContext(), "server ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case FLAG_TRANSFER_PROGRESS:
+                    int progress = msg.arg1;
+                    builder.setContentTitle("Transfering...")
+                            .setProgress(100, progress, false)
+                            .setWhen(System.currentTimeMillis());
+                    Notification notification = builder.build();
+                    notification.flags = Notification.FLAG_AUTO_CANCEL;
+                    notificationManager.notify(NOTIFY_ID, notification);
+                    break;
+				case FLAG_SHOW_NOTIFICATION:
+					setNotification();
+					break;
 
+            }
         }
-    }
-	
+    };
+
 	public FileTransferService(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
@@ -80,6 +93,7 @@ public class FileTransferService extends IntentService {
 			String filePath = intent.getExtras().getString(EXTRA_FILE_PATH);
 			String host = intent.getExtras().getString(EXTRA_GROUP_OWNER_ADDRESS);
 			int port = intent.getExtras().getInt(EXTRA_GROUP_OWNER_PORT);
+			handler.sendEmptyMessage(FLAG_SHOW_NOTIFICATION);
 			Socket socket = new Socket();
 			try {
 				socket.bind(null);
@@ -88,11 +102,21 @@ public class FileTransferService extends IntentService {
 				OutputStream os = socket.getOutputStream();
 				os.write(Utils.intToByteArray(file.getName().length()));
 				os.write(file.getName().getBytes("UTF-8"));
+				os.write(Utils.longToByteArray(file.length()));
+				os.flush();
 				FileInputStream fis = new FileInputStream(file);
 				int len = 0;
 				byte[] buffer = new byte[1024];
+				long writeLen = 0;
+				int progress = 0;
 				while ((len = fis.read(buffer)) != -1) {
 					os.write(buffer, 0, len);
+					writeLen += len;
+					int tempProgress = (int) (writeLen * 100 / file.length());
+					if (progress != tempProgress) {
+						handler.sendMessage(handler.obtainMessage(FLAG_TRANSFER_PROGRESS, tempProgress, 0));
+						progress = tempProgress;
+					}
 				}
 				os.flush();
 				os.close();
@@ -119,7 +143,7 @@ public class FileTransferService extends IntentService {
 				ServerSocket serverSocket = new ServerSocket(port);
 				handler.sendEmptyMessage(FLAG_READY_TO_SERVER);
 				Socket socket = serverSocket.accept();
-
+				handler.sendEmptyMessage(FLAG_SHOW_NOTIFICATION);
 				InputStream is = socket.getInputStream();
 
 				byte[] u4 = new byte[4];
@@ -133,13 +157,22 @@ public class FileTransferService extends IntentService {
 				if (!saveFile.getParentFile().exists()) {
 					saveFile.getParentFile().mkdir();
 				}
+                byte[] u8 = new byte[8];
+                is.read(u8);
+                long fileLen = Utils.byteArrayToLong(u8);
 				FileOutputStream fos = new FileOutputStream(saveFile);
 				int len = 0;
-				int readLen = 0;
+				long readLen = 0;
+				int progress = 0;
 				byte[] buffer = new byte[1024*10];
 				while ((len = is.read(buffer)) != -1) {
 					fos.write(buffer, 0, len);
 					readLen += len;
+					int tempProgress = (int) (readLen * 100 / fileLen);
+                    if (progress != tempProgress) {
+                        handler.sendMessage(handler.obtainMessage(FLAG_TRANSFER_PROGRESS, tempProgress, 0));
+						progress = tempProgress;
+                    }
 				}
 				System.out.println("readlen="+readLen);
 				fos.flush();
@@ -155,5 +188,35 @@ public class FileTransferService extends IntentService {
 			}
 		}
 	}
+
+	private static final int NOTIFY_ID = R.string.app_name;
+	private static final String CHANNEL = "transfer";
+
+	private NotificationManager notificationManager;
+	private NotificationCompat.Builder builder;
+
+    private void setNotification() {
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            builder = new NotificationCompat.Builder(this, CHANNEL);
+            builder.setContentTitle("Start transfer")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                    .setOngoing(true)
+                    .setAutoCancel(true)
+                    .setWhen(System.currentTimeMillis());
+            notificationManager.notify(NOTIFY_ID, builder.build());
+        }
+    }
+
+    private void complete() {
+        if (builder != null) {
+            builder.setContentTitle("Transfer Complete");
+            Notification notification = builder.build();
+            notification.flags = Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(NOTIFY_ID, notification);
+        }
+        stopSelf();
+    }
 
 }
